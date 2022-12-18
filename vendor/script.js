147f76d1277673995e9b79552d29a1e6b2ace03e
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2022-12-18 15:09:27 +0500

    все сверстал, написал и рассортировал по папкам в соответствии с БЭМ

--- vendor/script.js
+++ vendor/script.js
@@ -1,26 +1,23 @@
-let page = document.querySelector('.page');
-let nameProfile = page.querySelector('.profile__name');
-let jobProfile = page.querySelector('.profile__job');
-let editButton = page.querySelector('.profile__edit-button');
+let page = document.querySelector(".page");
+let nameProfile = page.querySelector(".profile__name");
+let jobProfile = page.querySelector(".profile__job");
+let editButton = page.querySelector(".profile__edit-button");
 
-let closeButton = page.querySelector('.popup__close-button');
+let closeButton = page.querySelector(".popup__close-button");
 
-let popup = page.querySelector('.popup');
-
+let popup = page.querySelector(".popup");
 
 // Находим форму в DOM
-let formElement = popup.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
+let formElement = popup.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__job'); // Воспользуйтесь инструментом .querySelector()
-
-
+let nameInput = formElement.querySelector(".popup__name"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".popup__job"); // Воспользуйтесь инструментом .querySelector()
 
 // Функция открытия Popup
 function showPopup() {
   // console.log(popup.classList);
-  popup.classList.add('popup_opened'); // Показываю попап: Добавляю класс с display flex к оверлею
-  page.classList.add('noscroll'); // Убираю скролл у окна
+  popup.classList.add("popup_opened"); // Показываю попап: Добавляю класс с display flex к оверлею
+  page.classList.add("noscroll"); // Убираю скролл у окна
 
   //console.log(nameProfile.textContent);
   //console.log(jobProfile.innerHTML);
@@ -29,34 +26,35 @@ function showPopup() {
 
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
+
+  // Закрываем попап без сохранения при нажатии Esc. ХЗ как оно работает, мы это еще не проходили
+  // https://stackoverflow.com/questions/1481626/how-to-handle-esc-keydown-on-javascript-popup-window)
+  // Надо как-то отключить выполнение этого правила/скрипта, после того как окно (попап) закрыто.
+  // Сейчас в консоли (escape pressed) считается количество выполнений/нажатий Esc
+  window.onkeydown = function (event) {
+    if (event.keyCode == 27) {
+      //console.log("escape pressed");
+      closePopup();
+    }
+  };
 }
-editButton.addEventListener('click', showPopup); // Отслеживаем событие "клик по кнопке правка"
+editButton.addEventListener("click", showPopup); // Отслеживаем событие "клик по кнопке правка"
 
 // Функция закрытия Popup
 function closePopup() {
-  if (popup.classList.contains('popup_opened')){
-    popup.classList.remove('popup_opened'); // Скрываю попап: Убираю класс с display flex к оверлею - остается display none
+  if (popup.classList.contains("popup_opened")) {
+    popup.classList.remove("popup_opened"); // Скрываю попап: Убираю класс с display flex к оверлею - остается display none
   }
-  if (page.classList.contains('noscroll')){
-    page.classList.remove('noscroll'); // Возвращаю скролл у окна
+  if (page.classList.contains("noscroll")) {
+    page.classList.remove("noscroll"); // Возвращаю скролл у окна
   }
 }
+closeButton.addEventListener("click", closePopup); // Отслеживаем событие "клик по кнопке попапа закрыть"
 
-closeButton.addEventListener('click', closePopup); // Отслеживаем событие "клик по кнопке попапа закрыть"
-
-// Закрываем попап без сохранения при нажатии Esc
-// https://stackoverflow.com/questions/1481626/how-to-handle-esc-keydown-on-javascript-popup-window)
-window.onkeydown = function( event ) {
-  if ( event.keyCode == 27 ) {
-    //console.log( 'escape pressed' );
-    closePopup();
-  }
-};
-
-function handleFormSubmit (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
+// Шаблон функции из Задания
+function handleFormSubmit(evt) {
+  // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
+  evt.preventDefault();
   // Получите значение полей jobInput и nameInput из свойства value
   // Выберите элементы, куда должны быть вставлены значения полей
   // Вставьте новые значения с помощью textContent
@@ -67,5 +65,4 @@ function handleFormSubmit (evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
-
+formElement.addEventListener("submit", handleFormSubmit);
